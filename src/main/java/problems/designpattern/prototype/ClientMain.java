75d6cd12e33e97f11e9294ed2d99707414a512d8
sandesh_kumar <sandesh_kumar@EPINGUGW0445>
2024-02-21 07:56:05 +0530

    added file

---
+++ src/main/java/problems/designpattern/prototype/ClientMain.java
@@ -0,0 +1,9 @@
+package problems.designpattern.prototype;
+
+public class ClientMain {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/main/java/problems/designpattern/singleton/package-info.java
@@ -0,0 +1,1 @@
+package problems.designpattern.singleton;
